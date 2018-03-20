3e9661c94c938f45135f5be069b4b9bafd97047a
Margot <vmargotpaez@gmail.com>
2018-03-19 21:19:04 -0400

    making stripe live

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -3,6 +3,6 @@
 
   $("#amt").on("keyup paste", function() {
     var amt = $(this).val()*100;
-    $("#stripe").html("<script id=\"stripe\" src=\"https://checkout.stripe.com/checkout.js\" class=\"stripe-button\" data-key=\"pk_test_6pRNASCoBOKtIshFeQd4XMUh\" data-description=\"Donation to Advance Progress\" data-amount=\"" + amt + "\" data-locale=\"auto\"></script>");
+    $("#stripe").html("<script id=\"stripe\" src=\"https://checkout.stripe.com/checkout.js\" class=\"stripe-button\" data-key=\"pk_live_dwVbjMBEoByGdfMGR8SZrW1d\" data-description=\"Donation to Advance Progress\" data-amount=\"" + amt + "\" data-locale=\"auto\"></script>");
   });
 }(jQuery));
