ea66cc355fd2a76d13956963db22449b608fdfdd
Margot <vmargotpaez@gmail.com>
2018-04-22 21:23:49 -0400

    added some exception handling to strip controller; updated copy for donation page

--- app/Http/Controllers/Stripe/StripeController.php
+++ app/Http/Controllers/Stripe/StripeController.php
@@ -37,14 +37,61 @@ class StripeController extends Controller
           'source'  => $token
       ));
 
-      $charge = \Stripe\Charge::create(array(
-          'customer' => $customer->id,
-          'amount'   => $amount,
-          'currency' => 'usd'
-      ));
+
+
+      try {
+        // Use Stripe's library to make requests...
+        $charge = \Stripe\Charge::create(array(
+            'customer' => $customer->id,
+            'amount'   => $amount,
+            'currency' => 'usd'
+        ));
+        $success = 1;
+      } catch(\Stripe\Error\Card $e) {
+        // Since it's a decline, \Stripe\Error\Card will be caught
+        $body = $e->getJsonBody();
+        $err  = $body['error'];
+
+      /*  print('Status is:' . $e->getHttpStatus() . "\n");
+        print('Type is:' . $err['type'] . "\n");
+        print('Code is:' . $err['code'] . "\n");
+        // param is '' in this case
+        print('Param is:' . $err['param'] . "\n");
+        print('Message is:' . $err['message'] . "\n"); */
+      } catch (\Stripe\Error\RateLimit $e) {
+        // Too many requests made to the API too quickly
+        $body = $e->getJsonBody();
+        $err  = $body['error'];
+      } catch (\Stripe\Error\InvalidRequest $e) {
+        // Invalid parameters were supplied to Stripe's API
+        $body = $e->getJsonBody();
+        $err  = $body['error'];
+      } catch (\Stripe\Error\Authentication $e) {
+        // Authentication with Stripe's API failed
+        // (maybe you changed API keys recently)
+        $body = $e->getJsonBody();
+        $err  = $body['error'];
+      } catch (\Stripe\Error\ApiConnection $e) {
+        // Network communication with Stripe failed
+        $body = $e->getJsonBody();
+        $err  = $body['error'];
+      } catch (\Stripe\Error\Base $e) {
+        // Display a very generic error to the user, and maybe send
+        // yourself an email
+        $body = $e->getJsonBody();
+        $err  = $body['error'];
+      } catch (Exception $e) {
+        // Something else happened, completely unrelated to Stripe
+        $body = $e->getJsonBody();
+        $err  = $body['error'];
+      }
 
       //return view('donate', ['result' =>  'Successfully charged $50.00!'] );
-      return view('confirm');
+      if ($success == 1) {
+        return view('confirm');
+      } else {
+        return session()->flash('status','Error: ' . $err['message']);
+      }
     }
 
 }
--- resources/views/donate.blade.php
+++ resources/views/donate.blade.php
@@ -8,7 +8,16 @@ Donate
 Donate
 @endsection
 
+@section('left-side')
+Advance Progress aims to be fully grassroots funded. Our goal is to reach 1,800 small donors pledging $5 to $15 per month. This funding source allows us to do our work without being compromised by the corporate money other think tanks rely on. 
+@endsection
+
 @section('content')
+@if (session('status'))
+    <div class="alert alert-info">
+        {{ session('status') }}
+    </div>
+@endif
 {!! Form::open() !!}
 {!! Form::hidden('test', 'testing') !!}
 {!! Form::label('amount','How much would you like to donate? ') !!}
