a855716342cea07a73a0a1cad77e49569de0f3b4
Luke Policinski <luke@lukepolo.com>
2018-05-08 15:04:48 -0400

    Made the about section cleaner

--- resources/views/bulma/leap-home.blade.php
+++ resources/views/bulma/leap-home.blade.php
@@ -26,47 +26,63 @@
                 <h2 class="subtitle">
                    American Prisoner's disenfranchisement is a problem. Let's fix it.
                 </h2>
-                <a href ="{{ url('/project') }}" class="button is-primary">Learn How</a>
+                <a href ="https://peoplespolicyproject.org/2018/05/08/prisoners-should-be-allowed-to-vote-here-is-how-to-do-it/" class="button is-primary">Learn How</a>
             </div>
         </div>
     </div>
 </section>
-<section class="section">
+
+<section class="section" id ="about">
     <div class="container">
         <div class="heading">
-            <h1 class="title">Our Work</h1>
-            <h2 class="subtitle">Check out our projects</h2>
+            <h2 class="title is-2">Learn About Us</h2>
+            <p class="subtitle is-4">How We Can Help Advance Progress for You</p>
             <hr class ="title-line">
         </div>
-    </div>
-    <div class = "project">
-        <div class ="container">
-            <div class="container column is-10">
-                <div class="section">
 
-                    <div class="card">
+    <div class = "project">
+        <div class ="columns blue-bg">
+            <div class="column">
+                        <div class="card">
                         <div class="card-header"><p class="card-header-title">What We Do?</p></div>
                         <div class="card-content">
                             <div class="content">
                                 <ul>
                                     <li><strong>Drafting</strong> and creating model legislation implementing our progressive principles </li>
-                                    <li><strong>Policy</strong>analysis and research for various progressive policy ideas.</li>
+                                    <li><strong>Policy</strong> analysis and research for various progressive policy ideas.</li>
                                     <li><strong>Education</strong> about legislative processes, local government and civics.</li>
                                 </ul>
 
 
-                                <a href ="/project" class="button is-primary">Learn More</a>
                             </div>
 
+                    </div>
+                    <br />
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <div class="card-header"><p class="card-header-title">Why We Do It?</p></div>
+                    <div class="card-content">
+                        <div class="content">
+                            <ul>
+                                <li><strong>Solving Problems</strong>Activists know the issues, but sometimes need help solving it .</li>
+                                <li><strong>Holding the Powerful Accountable</strong>Legislatures don't work how they are supposed to work</li>
+                                <li><strong>Evening the field</strong>Corporations spend billions, hiring people to draft legislation that works for them.</li>
+                            </ul>
+
+
                         </div>
+
                     </div>
                     <br />
                 </div>
             </div>
 
 </section>
-
-
+<div class ="container column is-10" id ="join-us">
+<link href='https://actionnetwork.org/css/style-embed-whitelabel-v3.css' rel='stylesheet' type='text/css' /><script src='https://actionnetwork.org/widgets/v3/form/get-updates-30?format=js&source=widget&style=full'></script><div id='can-form-area-get-updates-30' style='width: 600px'><!-- this div is the target for our HTML insertion --></div>
+</div>
 </body>
 </html>
 
