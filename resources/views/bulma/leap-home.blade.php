db47d476e9da579068f255e5d1c4c708cee97d94
Luke Policinski <luke@lukepolo.com>
2018-05-08 15:21:39 -0400

    Just changed up the code a bit

--- resources/views/bulma/leap-home.blade.php
+++ resources/views/bulma/leap-home.blade.php
@@ -4,14 +4,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Landing - Free Bulma template</title>
+    <title>Advance Progress e</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <link rel="stylesheet" href="{{url('css/pages.css')}}">
     <meta property="og:type" content="website">
-    <meta property="og:title" content="Starter Template for bulma">
-    <meta property="og:description" content="Bulma is a CSS framework based on Flexbox and built with Sass.">
-    <meta property="og:site_name" content="Starter Template for bulma">
+    <meta property="og:title" content="Landing Page for Advanced Progress">
+    <meta property="og:description" content="Front Page">
+    <meta property="og:site_name" content="Advanced Progress">
 </head>
 <body>
 @include('bulma.navmenu')
